1ff4dd5b031f3b67d5a5356f5067301083781bcb
Mozamil Arefin Khan <101820486+mozamilarefinkhan@users.noreply.github.com>
2022-11-10 16:35:52 +0530

    Update Learn.js

--- src/Learn.js
+++ src/Learn.js
@@ -1,8 +1,49 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-const Learn = (_) => {
+const Learn = () => {
   const { id } = useParams();
-  return <p>This is the learn page for {id}</p>;
-};
+  const [carsData, setCarsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/cars.json")
+      .then((res) => {
+        setCarsData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+
+    <>
+
+
+      {
+        carsData.map((ele) => {
+          if (id === ele.id)
+            return (
+              
+<div  key={ele.id}>
+
+<p>{ele.bodyType}</p>
+<p>{ele.modelName}</p>
+<p>{ele.modelType}</p>
+<img src={ele.imageUrl} alt="img" style={{width:"350px", height:"350px"}} />
+
+
+</div>
+                
+              
+            )
+        })
+      }
+
+    </>
+
+  )
+}
 export default Learn;
